cf6bd02fe33a9c8b4bbaf021c22738f3678aaa01
jiinkang0514 <crunkgraph@gmail.com>
2025-05-30 14:58:22 +0900

    Update main.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,1 +1,11 @@
-console.log("this works");
+const API_KEY = config.WEATHER_API_KEY;
+
+function getWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+    let local_weather_data = data;
+    console.log(local_weather_data);
+
+getWeatherData(); // ← actually calling the function
