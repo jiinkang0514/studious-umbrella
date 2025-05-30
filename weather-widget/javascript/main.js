2aa24500490e7b3a8ac61a4d20eb63b8b91e550d
jiinkang0514 <crunkgraph@gmail.com>
2025-05-30 17:50:43 +0900

    Update main.js logic for dynamic zip input

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,11 +1,22 @@
-const API_KEY = config.WEATHER_API_KEY;
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
 
-function getWeatherData() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+getWeatherData = (zip) => {
+    let API_KEY = config.WEATHER_API_KEY;
+    let API_ENDPOINT = '`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}}';
+
+    fetch(API_ENDPOINT)
+    .then((response => response.json()))
+    .then((data) => {
+        let local_weather_data = data;
+        console.log(local_weather_data)
+    })
 }
-    let local_weather_data = data;
-    console.log(local_weather_data);
 
-getWeatherData(); // ← actually calling the function
+const getZipCode = (e) => {
+    e.preventDefault();
+    let ZIP_CODE = input.value;
+    getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener("click", getZipCode);
